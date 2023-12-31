c9bebb6734f90397d45798e7c7e10020ae53e162
Marco177171 <msebastiani93@gmail.com>
2023-12-31 22:37:06 +0100

    update

---
+++ docker/installDocker/uninstallDocker.sh
@@ -0,0 +1,5 @@
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+echo DOCKER UNINSTALLED
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+echo DOCKER SYSTEM REPOS REMOVED
