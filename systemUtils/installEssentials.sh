5fd6eef86ca7edd260ab04062c34d437a9313f83
Marco177171 <msebastiani93@gmail.com>
2024-01-06 18:48:46 +0100

    update

---
+++ systemUtils/installEssentials.sh
@@ -0,0 +1,5 @@
+echo INSTALLING BASIC MUST HAVE SOFTWARE
+sudo apt update && sudo apt upgrade -y
+echo SYSTEM UPDATED
+sudo apt install -y ssh wget curl tree neofetch vim net-tools
+echo ESSENTIAL INSTALLED
