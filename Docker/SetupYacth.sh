c6ed88fe5b2cc11e2ab5c7180696b85ac94e0ed7
Marco177171 <msebastiani93@gmail.com>
2024-01-23 19:59:03 +0000

    yacht update (name)

--- Docker/SetupYacth.sh
+++ Docker/SetupYacth.sh
@@ -1,4 +1,4 @@
 docker volume create yacht
-echo yacht volume crated
-docker run -d -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config selfhostedpro/yacht
+echo yacht volume created
+docker run -d -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config --name yacht selfhostedpro/yacht
 echo yacht started. Available on http://localhost:8000
