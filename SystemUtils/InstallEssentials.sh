528f8ee99437e3721462ebdf66d84d9b24c9e879
Marco177171 <msebastiani93@gmail.com>
2024-01-26 16:08:56 +0100

    update

--- SystemUtils/InstallEssentials.sh
+++ SystemUtils/InstallEssentials.sh
@@ -5,7 +5,7 @@ echo INSTALLING BASIC MUST HAVE SOFTWARE
 sudo apt update && sudo apt upgrade -y
 echo SYSTEM UPDATED
 
-sudo apt install -y ssh git wget curl tree neofetch vim net-tools powertop htop gnome-tweaks gnome-shell-extension-manager tilix
+sudo apt install -y ssh git wget curl tree neofetch vim net-tools powertop htop gnome-tweaks gnome-shell-extension-manager tilix filezilla
 sudo snap install brave discord insomnia mailspring slack spotify loupe onlyoffice-desktopeditors trello-desktop
 
 echo ESSENTIALS INSTALLED
