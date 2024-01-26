9f1b0c8848dcbdab5e06decbb2cca741b1a4399b
Marco177171 <msebastiani93@gmail.com>
2024-01-26 15:54:08 +0100

    apps added

--- SystemUtils/InstallEssentials.sh
+++ SystemUtils/InstallEssentials.sh
@@ -5,8 +5,7 @@ echo INSTALLING BASIC MUST HAVE SOFTWARE
 sudo apt update && sudo apt upgrade -y
 echo SYSTEM UPDATED
 
-sudo apt install -y ssh git wget curl tree neofetch vim net-tools powertop htop
-
-sudo snap install brave discord insomnia mailspring slack spotify code loupe onlyoffice-desktopeditors trello-desktop
+sudo apt install -y ssh git wget curl tree neofetch vim net-tools powertop htop gnome-tweaks gnome-shell-extension-manager tilix
+sudo snap install brave discord insomnia mailspring slack spotify loupe onlyoffice-desktopeditors trello-desktop
 
 echo ESSENTIALS INSTALLED
