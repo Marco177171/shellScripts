c66192b74f81e9d6acb52e5f3f1b93ab5ddee2e0
Marco177171 <msebastiani93@gmail.com>
2024-01-26 16:33:10 +0100

    update

--- SystemUtils/InstallEssentials.sh
+++ SystemUtils/InstallEssentials.sh
@@ -6,6 +6,7 @@ sudo apt update && sudo apt upgrade -y
 echo SYSTEM UPDATED
 
 sudo apt install -y ssh git wget curl tree neofetch vim net-tools powertop htop gnome-tweaks gnome-shell-extension-manager tilix filezilla
-sudo snap install brave discord insomnia mailspring slack spotify loupe onlyoffice-desktopeditors trello-desktop
+
+sudo snap install brave discord insomnia mailspring slack spotify loupe onlyoffice-desktopeditors trello-desktop inkscape laidout gimp
 
 echo ESSENTIALS INSTALLED
