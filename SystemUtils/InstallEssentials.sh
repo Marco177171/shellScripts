a7d51099d726b5317adce085fe475a74ff5fa103
Marco177171 <msebastiani93@gmail.com>
2024-01-26 11:39:23 +0100

    desktop essentials updated

--- SystemUtils/InstallEssentials.sh
+++ SystemUtils/InstallEssentials.sh
@@ -5,5 +5,8 @@ echo INSTALLING BASIC MUST HAVE SOFTWARE
 sudo apt update && sudo apt upgrade -y
 echo SYSTEM UPDATED
 
-sudo apt install -y ssh wget curl tree neofetch vim net-tools
-echo ESSENTIAL INSTALLED
+sudo apt install -y ssh git wget curl tree neofetch vim net-tools powertop htop
+
+sudo snap install brave discord insomnia mailspring slack spotify code loupe onlyoffice-desktopeditors trello-desktop
+
+echo ESSENTIALS INSTALLED
