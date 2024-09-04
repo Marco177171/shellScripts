a4c973e5c690454f576974d10e28a7bb4309ef7e
Marco177171 <msebastiani93@gmail.com>
2024-09-04 18:17:39 +0200

    update

---
+++ Docker/Backup/backup_docker_volumes.sh
@@ -0,0 +1,16 @@
+#echo insert the name of the volume you want to backup
+
+mount /dev/sdb1 mount_point #change drive if necessary
+
+for volume in $(docker volume ls --quiet)
+do
+	echo backing up $volume ...
+	docker run -d --rm -v "$volume":/backup-volume -v "$(pwd)":/backup busybox tar -zcvf /backup/"$volume"-"$(date '+%Y-%m-%d')"-backup.tar.gz /backup-volume
+	cp "$volume"-"$(date '+%Y-%m-%d')"-backup.tar.gz ./mount_point
+	rm -rf "$volume"-"$(date '+%Y-%m-%d')"-backup.tar.gz
+done
+
+umount /dev/sdb1
+
+echo backup drive unmounted
+echo backup complete
