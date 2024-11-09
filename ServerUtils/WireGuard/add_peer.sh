4be97261cf717a6ed7c83fb69b937a46201e0297
Marco177171 <msebastiani93@gmail.com>
2024-11-09 12:33:09 +0100

    filebrowser added to docker services

--- ServerUtils/WireGuard/add_peer.sh
+++ ServerUtils/WireGuard/add_peer.sh
@@ -3,13 +3,13 @@ echo "launch 'sudo wg' on the machine you want to add. Insert:"
 echo "the public key..."
 read PEER_PUBLIC_KEY
 
-echo "the machine's ip on the wg0 interface..."
-read PEER_WIREGUARD_IP
+echo "set allowed physical ip..."
+read ALLOWED_IP
 
-echo "the machine's physical ip..."
-read PEER_PHYSICAL_IP
+echo "set the endpoint ip..."
+read ENDPOINT
 
 echo "the port..."
 read PEER_PORT
 
-wg set wg0 peer $PEER_PUBLIC_KEY allowed-ips $PEER_WIREGUARD_IP endpoint $PEER_PHYSICAL_IP:$PEER_PORT
+wg set wg0 peer $PEER_PUBLIC_KEY allowed-ips $ALLOWED_IP endpoint $ENDPOINT:$PEER_PORT
